1f4218a6dd548ff90a708160dea7181697c8dd7a
vboxuser <vboxuser@databox.myguest.virtualbox.org>
2024-12-06 19:40:44 +0100

    add: basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { WalletPage } from "./pages/WalletPage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegistrationPage } from "./pages/RegistrationPage";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WalletPage />}/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
