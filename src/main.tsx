e56f305e9f4fc2a9511043b13f3e1f1e1843fe88
migueldcdev <migueldc.dev@protonmail.com>
2024-12-07 00:55:06 +0000

    add: redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Provider } from "./components/ui/provider.tsx";
+import { Provider as ChakraProvider } from "./components/ui/provider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </StrictMode>,
 );
