316d43a36ce296f704a5b380bfcf8d21fac1d55f
migueldcdev <migueldc.dev@protonmail.com>
2024-12-07 20:14:50 +0000

    add: creating wallet

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,8 +1,48 @@
+import { Box, Text, Flex, IconButton } from "@chakra-ui/react";
+import { MdArrowOutward } from "react-icons/md";
+import { LuPlus } from "react-icons/lu";
 
 export const Wallet = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+    const today = new Date();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    }).format(today);
+
+  return (
+    <Box background="gray.300" minWidth="1/3" borderRadius="md" paddingX="1">
+      <Box background="gray.600" borderBottomRadius="xl" color="gray.200" padding="1">
+        <Text>Welcome back,</Text>
+        <Text>Miguel!</Text>
+        <Text>{formattedDate}</Text>
+      </Box>  
+      <Text marginTop="8" textStyle="4xl" fontWeight="bold">
+        $1,000.63
+      </Text>
+      <Text textStyle="xs" color="gray.500">
+        USD ACCOUNT BALANCE
+      </Text>
+      <Flex marginTop="6" gap="3">
+        <IconButton
+          background="white"
+          color="gray.500"
+          borderRadius="full"
+          paddingX="2"          
+        >
+          <LuPlus />  
+          <Text>Add</Text>
+        </IconButton>
+        <IconButton
+          background="white"
+          color="gray.500"
+          borderRadius="full"
+          paddingX="2"
+        >
+          <MdArrowOutward />
+          Send
+        </IconButton>
+      </Flex>
+    </Box>
+  );
+};
--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,6 +1,7 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Center } from "@chakra-ui/react";
 
 import { LogoutButton } from "@/components/LogoutButton";
+import { Wallet } from "@/components/Wallet";
 
 export const WalletPage = () => {
   return (
@@ -8,6 +9,9 @@ export const WalletPage = () => {
       <Flex justify="flex-end" padding={2}>
         <LogoutButton />
       </Flex>
+      <Center>
+        <Wallet />
+      </Center>
     </>
   );
 };
