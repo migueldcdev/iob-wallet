ae3c36cfe67abe34b204a93f5a3798dae9d13e13
migueldcdev <migueldc.dev@protonmail.com>
2024-12-07 17:33:31 +0000

    do: refactor logout button

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -1,20 +1,14 @@
 import { IconButton } from "@chakra-ui/react";
 import { FiLogOut } from "react-icons/fi";
-//import { useNavigate } from "react-router-dom";
 
 import { useAppDispatch } from "@/app/hooks";
 import { logout } from "@/features/auth/authSlice";
 
 export const LogoutButton = () => {
   const dispatch = useAppDispatch();
-  //const navigate = useNavigate();
-
-  function handleLogout() {
-    dispatch(logout());
-  }
 
   return (
-    <IconButton onClick={handleLogout}>
+    <IconButton onClick={() => dispatch(logout())}>
       <FiLogOut />
     </IconButton>
   );
