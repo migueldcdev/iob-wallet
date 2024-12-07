7e26dff44202dce42292eed8ab95a0f4682ad900
migueldcdev <migueldc.dev@protonmail.com>
2024-12-07 17:42:18 +0000

    add: random id for wallet

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import walletReducer from "../features/wallet/walletSlice"
+import walletReducer from "../features/wallet/walletSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    wallet: walletReducer
+    wallet: walletReducer,
   },
 });
 
--- src/features/wallet/walletSlice.tsx
+++ src/features/wallet/walletSlice.tsx
@@ -1,41 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
 
 type Transaction = {
-    id: string;
-    from: string;
-    to: string;
-    amount: number;
-}
+  id: string;
+  from: string;
+  to: string;
+  amount: number;
+};
 
 type Wallet = {
-    id: string;
-    user: string;
-    balance: number;
-    transactions: Transaction[]
-}
+  id: string;
+  user: string;
+  balance: number;
+  transactions: Transaction[];
+};
 
 interface WalletState {
-    wallets: Wallet[]
+  wallets: Wallet[];
 }
 
 const initialState: WalletState = {
-    wallets: []
-}
+  wallets: [],
+};
 
 const walletSlice = createSlice({
-    name: "wallet",
-    initialState,
-    reducers: {
-        createWallet: (state, action: PayloadAction<string>) => {
-            state.wallets.push({
-                id: "",
-                user: action.payload,
-                balance: 0,
-                transactions: []
-            })
-        }
-    }
-})
+  name: "wallet",
+  initialState,
+  reducers: {
+    createWallet: (state, action: PayloadAction<string>) => {
+      state.wallets.push({
+        id: uuidv4(),
+        user: action.payload,
+        balance: 0,
+        transactions: [],
+      });
+    },
+  },
+});
 
 export const { createWallet } = walletSlice.actions;
 export default walletSlice.reducer;
