33e0e0e943e5b905281356592b6be84ceedcc94e
migueldcdev <migueldc.dev@protonmail.com>
2024-12-09 22:10:42 +0000

    add: login form test suite

---
+++ src/components/LoginForm/index.test.tsx
@@ -0,0 +1,64 @@
+import { describe, test } from "vitest";
+import { LoginForm } from ".";
+import { renderWithProviders } from "@/test-utils";
+// wrap in BrowserRoute to fix: useNavigate() may be
+//used only in the context of a <Router> component.
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/dom";
+
+const initialState = {
+  auth: {
+    users: [
+      {
+        id: "4cedda05-4d57-4837-b66f-92d357c22d1b",
+        name: "John",
+        email: "user@mail.com",
+        password: "abc123",
+      },
+    ],
+    currentUser: null,
+  },
+  wallet: {
+    wallets: [
+      {
+        id: "c9301bf2-d26b-4d4e-a22b-9d594bd4b873",
+        user: "user@mail.com",
+        balance: 100,
+        transactions: [],
+      },
+    ],
+  },
+};
+
+describe("Login form unit test", () => {
+  test("handles user login", async () => {
+    renderWithProviders(
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>,
+      {
+        preloadedState: {
+          auth: initialState.auth,
+          wallet: initialState.wallet,
+        },
+      },
+    );
+
+    const user = userEvent.setup();
+
+    const inputEmail = screen.getByLabelText("Email");
+
+    await user.type(inputEmail, "user@mail.com");
+
+    const inputPassword = screen.getByLabelText("Password");
+
+    await user.type(inputPassword, "abc123");
+
+    const loginButton = screen.getByRole("button", { name: "Log in" });
+
+    await user.click(loginButton);
+
+    //todo
+  });
+});
