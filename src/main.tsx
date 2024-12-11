777f9c69f3dc74a77633b4f88bddb9e867efa962
migueldcdev <migueldc.dev@protonmail.com>
2024-12-11 19:01:39 +0000

    do: unique light theme

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from "./app/store.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
+      <ChakraProvider forcedTheme="light">
         <App />
       </ChakraProvider>
     </Provider>
